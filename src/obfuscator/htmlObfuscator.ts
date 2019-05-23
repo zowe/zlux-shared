37521f9b3bf746e3c351dceb5ce585bba6d2adad
1000TurquoisePogs <sgrady@rocketsoftware.com>
2019-05-23 18:13:56 -0400

    Regex quantity fixes
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- src/obfuscator/htmlObfuscator.ts
+++ src/obfuscator/htmlObfuscator.ts
@@ -15,8 +15,8 @@ export class HtmlObfuscator {
   }
 
   public findAndReplaceHTMLEntities(str: string): string {
-    let regexHTMLEnd   = new RegExp('<\/[A-Za-z0-9]*\\s*>');
-    let regexHTMLStart = new RegExp('<[A-Za-z0-9]*(\\s*\\S*)*>|<[A-Za-z0-9]*(\\s*)>');
+    let regexHTMLEnd   = new RegExp('<\/[A-Za-z0-9]+\\s?>');
+    let regexHTMLStart = new RegExp('<[A-Za-z0-9]+(\\s+\\S*)?>|<[A-Za-z0-9]+(\\s?)>');
 
     var newString = str;
 
