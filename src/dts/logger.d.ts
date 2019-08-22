e78f3c73ec3f157ff890f82e560754198140a6d6
Leanid Astrakou <lastrakou@rocketsoftware.com>
2019-08-22 16:10:40 -0400

    Re-added copyright header
    Signed-off-by: Leanid Astrakou <lastrakou@rocketsoftware.com>
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- src/dts/logger.d.ts
+++ src/dts/logger.d.ts
@@ -1,3 +1,13 @@
+
+ /*	
+  This program and the accompanying materials are	
+  made available under the terms of the Eclipse Public License v2.0 which accompanies	
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html	
+  	
+  SPDX-License-Identifier: EPL-2.0	
+  	
+  Copyright Contributors to the Zowe Project.	
+*/
 export declare enum LogLevel {
     CRITICAL = 0,
     WARN = 1,
@@ -69,3 +79,12 @@ export declare class Logger implements ZLUX.Logger {
     makeComponentLogger(componentName: string, messages?: any): ComponentLogger;
     addLoggerMessages(componentName: string, messages: any): void;
 }
+ /*	
+  This program and the accompanying materials are	
+  made available under the terms of the Eclipse Public License v2.0 which accompanies	
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html	
+  	
+  SPDX-License-Identifier: EPL-2.0	
+  	
+  Copyright Contributors to the Zowe Project.	
+*/
