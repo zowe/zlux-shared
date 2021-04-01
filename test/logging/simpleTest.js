656eeee7efe1325fb46dadb3b5a63e4cf73dd8b6
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2021-04-01 15:43:48 +0200

    Added new optional parameter to logger.
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- test/logging/simpleTest.js
+++ test/logging/simpleTest.js
@@ -14,8 +14,10 @@ function testDuplicate(logger) {
   logger.makeComponentLogger('foo');
   logger.makeComponentLogger('foo');
   /* should print
+    2021-03-18 13:01:08.808 <Test the logger:processID> userID WARN(_internal,simpleTest.js:15) Logger created with identical component name to pre-existing logger.
+    _messages overlap may occur.
 
-     [2018-11-29 17:54:43.395 WARNING] - Logger created with identical component name to pre-existing logger. Messages overlap may occur.
+    Note: the timestamp, the processID and userID are replaced by actual values.
   */
 }
 
@@ -27,9 +29,10 @@ function testInfo(logger) {
     console.log('testInfo failed, e='+e);
   }
   /* should print
+    2021-03-18 13:01:08.810 <Test the logger:processID> userID INFO (testInfo,simpleTest.js:25) FYI this worked
 
-     [2018-11-29 17:54:43.400 testInfo INFO] - FYI this worked
-   */
+    Note: the timestamp, the processID and userID are replaced by actual values.
+  */
 }
 
 function testArguments(logger) {
@@ -41,19 +44,19 @@ function testArguments(logger) {
   let myobject = {message: 'this should look familiar', mystring: mystring, myint: myint, myfunction: myfunction, myboolean: myboolean};
   log.info('Look at my string', mystring, ', and my int', myint, ', and my boolean', myboolean, ', and my function', myfunction, ', and finally my object', myobject);
   /* should print
+    2021-03-18 13:01:08.811 <Test the logger:processID> userID INFO (testArguments,simpleTest.js:42) Look at my string here it is , and my int 45 , and my boolean true , and my function function(num1, num2) {return num1+num2;} , and finally my object { message: 'this should look familiar',
+    mystring: 'here it is',
+    myint: 45,
+    myfunction: [Function: myfunction],
+    myboolean: true }
 
-     [2018-11-29 17:54:43.401 testArguments INFO] - Look at my string here it is , and my int 45 , and my boolean true , and my function function(num1, num2) {return num1+num2;} , and finally my object { message: 'this should look familiar',
-     mystring: 'here it is',
-     myint: 45,
-     myfunction: [Function: myfunction],
-     myboolean: true }
-
-   */
+    Note: the timestamp, the processID and userID are replaced by actual values.  
+  */
 }
 
 function runTests() {
   let logger = new logModule.Logger();
-  logger.addDestination(logger.makeDefaultDestination(true,true,true));
+  logger.addDestination(logger.makeDefaultDestination(true,true,true,true,true,"Test the logger"));
   testDuplicate(logger);
   testInfo(logger);
   testArguments(logger);
